a5a0b459ab9ac534345602d31ae341f76ad78a24
Bridgette <turner.bridgette@yahoo.com>
2020-11-10 15:58:35 -0700

    ready for deploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || '',
-    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || '',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://bridgette:Renly2357@localhost/bobsburgers',
 }
