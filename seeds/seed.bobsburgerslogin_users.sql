a5eaa4ffb98e88fd53688a371064fe5f608e31bd
Bridgette <turner.bridgette@yahoo.com>
2020-11-10 19:08:00 -0700

    trying to seed

--- seeds/seed.bobsburgerslogin_users.sql
+++ seeds/seed.bobsburgerslogin_users.sql
@@ -1,3 +1,10 @@
+BEGIN;
+
+TRUNCATE users RESTART IDENTITY CASCADE;
+
 INSERT INTO users ( name, email, password ) 
     VALUES 
-    ('Bob', 'Bob@bobsburgers.com', '$2b$10$Iy2G/sbfNY4YdMrXwxCJMetfoEXOjlLJxOQN5sK2SSiQPTnpJ55xa'); //Burg3z
+    ('Bob', 'Bob@bobsburgers.com', '$2b$10$Iy2G/sbfNY4YdMrXwxCJMetfoEXOjlLJxOQN5sK2SSiQPTnpJ55xa'), //Burg3z
+    ('Louise', 'Louise@bobsburgers.com','$2b$10$RO/x8DiOhyW2eYqYfvenlOHCW9sM9nx4pcc7q54peqyCFRZFqlTmW'); //Booboo321
+
+    COMMIT;
