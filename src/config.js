a04807324c76dd82adc2be1ff3cfff7be3dcb314
Bridgette <turner.bridgette@yahoo.com>
2020-11-11 08:49:39 -0700

    another try

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://bridgette:Renly2357@localhost/bobsburgers',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://lyopaomrthrbsr:1890e01796bbe521bb7ead829f6fcb24daf7a205a67eaba240dd075fc219223f@ec2-52-206-15-227.compute-1.amazonaws.com:5432/dddf69k4gdgrso',
 }
