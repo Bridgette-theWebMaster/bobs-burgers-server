4bf00bdbea125227a1b06109d62e267e458bc5ce
Bridgette <turner.bridgette@yahoo.com>
2020-11-10 17:57:35 -0700

    disable node_modules cache

--- seeds/seed.bobsburgerslogin_users.sql
+++ seeds/seed.bobsburgerslogin_users.sql
@@ -1,3 +1,3 @@
 INSERT INTO users ( name, email, password ) 
     VALUES 
-    ('Bob', 'Bob@bobsburgers.com', 'Burg3rz!');
+    ('Bob', 'Bob@bobsburgers.com', '$2b$10$Iy2G/sbfNY4YdMrXwxCJMetfoEXOjlLJxOQN5sK2SSiQPTnpJ55xa'); //Burg3z
--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const {NODE_ENV} = require('./config')
+
+
 const authRouter = require("./routes/jwtAuth")
 const dashRouter = require('./routes/dashboard')
 
