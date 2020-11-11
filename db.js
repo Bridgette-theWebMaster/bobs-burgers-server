1da8761bc9695716501277d6c963dd4f604bc54b
Bridgette <turner.bridgette@yahoo.com>
2020-11-11 08:16:21 -0700

    fix db host

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Pool = require("pg").Pool
 const pool = new Pool({
     user: "bridgette",
     password: "Renly2357",
-    host: "localhost",
+    host: "postgres://lyopaomrthrbsr:1890e01796bbe521bb7ead829f6fcb24daf7a205a67eaba240dd075fc219223f@ec2-52-206-15-227.compute-1.amazonaws.com:5432/dddf69k4gdgrso",
     port: 5432,
     database: "bobsburgers"
 });
