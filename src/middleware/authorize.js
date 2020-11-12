387c8b31ca03a4cabb15c91c34295e4f89f5c79a
Bridgette <turner.bridgette@yahoo.com>
2020-11-12 13:40:31 -0700

    secret key fix

--- src/middleware/authorize.js
+++ src/middleware/authorize.js
@@ -9,7 +9,7 @@ module.exports = async(req, res, next) => {
         }
 
     try {
-        const payload = jwt.verify(token, jwtSecret)
+        const payload = jwt.verify(token, `${process.env.jwtSecret}`)
 
         req.user = payload.user;
         next()
