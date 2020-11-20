26bdc32ea7a9de8267dcea75af2402d57ac2074e
Bridgette <turner.bridgette@yahoo.com>
2020-11-19 20:22:01 -0700

    some user routes

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
-const Pool = require("pg").Pool
-
+const Pool = require("pg").Pool;
+/*
 const pool = new Pool({
     host: "ec2-52-206-15-227.compute-1.amazonaws.com",
     database: "dddf69k4gdgrso",
@@ -7,5 +7,13 @@ const pool = new Pool({
     port: 5432,
     password: "1890e01796bbe521bb7ead829f6fcb24daf7a205a67eaba240dd075fc219223f"
 });
+*/
 
+const pool = new Pool({
+  host: "localhost",
+  database: "bobsburgers",
+  user: "postgres",
+  port: 5432,
+  password: "",
+});
 module.exports = pool;
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,18 +1,53 @@
-const router = require('express').Router()
-const pool =  require('../../db')
-const authorize = require('../middleware/authorize')
+const router = require("express").Router();
+const pool = require("../../db");
+const bcrypt = require("bcrypt");
+const authorize = require("../middleware/authorize");
 
-router.get('/', authorize, async(req, res) => {
-    try {
+router.get("/", authorize, async (req, res) => {
+  try {
+    const user = await pool.query("SELECT name FROM users WHERE id = $1", [
+      req.user,
+    ]);
 
-        const user = await pool.query("SELECT name FROM users WHERE id = $1", [req.user])
+    res.json(user.rows[0]);
+  } catch (err) {
+    // console.error(err.message)
+    res.status(500).json("Server Error");
+  }
+});
 
-        res.json(user.rows[0])
-        
-    } catch(err) {
-       // console.error(err.message)
-        res.status(500).json("Server Error")
-    }
-})
+router.delete("/user/:id", authorize, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedUser = await pool.query("DELETE FROM users WHERE id = $1", [
+      id,
+    ]);
+
+    res.json("User deleted");
+  } catch (err) {
+    res.status(500).json("Server Error");
+  }
+});
+
+router.patch("/user/:id", authorize, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { password } = req.body;
+
+    const saltRound = 10;
+    const salt = await bcrypt.genSalt(saltRound);
+
+    const bcryptPassword = await bcrypt.hash(password, salt);
+    const updatedPassword = await pool.query(
+      "UPDATE users SET password = $1 WHERE id = $2",
+      [bcryptPassword, id]
+    );
+
+    res.json("Password updated");
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json("Server Error");
+  }
+});
 
 module.exports = router;
